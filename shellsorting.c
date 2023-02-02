8a5f66f69383ad7e65d32419c8a553c23fbccece
suvidhaks <32566719+suvidhaks@users.noreply.github.com>
2023-02-03 01:01:51 +0530

    Add files via upload

---
+++ shellsorting.c
@@ -0,0 +1,125 @@
+/* shellsort() is a sorting alg. with less number of iterations unlike bubble sort wich requires iteration of (n-1)(n-1)
+in shellsort alg. 
+given : input A[1..N]
+compute the middle number since array starts from 0 index
+set low=0;
+high=N-1;
+mid=(low+high)/2; 
+in the function void shellsort(int [], int);
+the first outer loop runs for n/2 elements every iterartion till it becomes zero
+e.g if N=10 ;
+(10/2=5); (5/2=2) ; (2/2=1); (1/2=0); (zero means there are no more elements to compare)
+so first outer loop runs for every n/2 elements until it becomes zero
+ for(mid=n/2;mid>0;mid/=2)  ---------------->runs for every n/2 elements till it becomes 0
+ for(i=mid;i<n;i++)  --------------> value of i is set to mid this loop iterates for the rightmost elements after mid
+ for(j=i-mid;j>=0 && v[j]>v[j+mid];j-=mid) -------------->  this  loop iterates for the left most elements  before mid
+ for every one iteration of i loop
+ j also iterates only once.
+ for N=5;
+ e.g: a[0], a[1], a[2], a[3], a[4]
+ mid=(0+4)/2=2;
+ for(i=mid;i<n;i++)
+ mid=2; i is set to mid i,e i=2; n=5; 
+  for(j=i-mid;j>=0 && v[j]>v[j+mid];j-=mid)
+  now, j=2-2=0
+  j=0 
+  now condition is checked if (a[j]>a[j+mid]) which means for every one element 
+  in the left side of mid is checked with exactly one element of the rightside of mid
+  first time when the loop starts a[0] is compared with a[mid] if a[0]>a[mid] it is swapped 
+  similary
+  a[1] is compared with a[3]
+  a[2] is compared with a[4]
+  if the number of elements are odd low=0 is compared with a[mid] && high=n-1 is also compared with a[mid] 
+
+bubble sorting is implemented in the function void sort(int [], int); 
+one can compare both the algorithms in terms of no.of iterations
+
+*/
+
+
+
+#include <stdio.h>
+#define LIMIT 100
+void read(int a[], int n);
+void shellsort(int [], int);
+void sort(int [], int);
+void display(int a[], int n);
+
+int main()
+{
+    int a[LIMIT], n;
+    printf("enter value of n");
+    scanf("%d", &n);
+     read(a, n);
+     //sort(a,n);
+     shellsort(a,n);
+     display(a,n);
+    return 0;
+}
+
+void read(int a[], int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    return ;
+}
+
+
+void sort(int a[], int n)
+{
+    
+    int i, j, temp=0;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    return ;
+}
+
+
+
+void shellsort(int v[], int n)
+{
+    int mid, i, j,  temp;
+    
+    for(mid=n/2;mid>0;mid/=2)
+    {
+        for(i=mid;i<n;i++)
+        {
+            
+            for(j=i-mid;j>=0 && v[j]>v[j+mid];j-=mid)
+            {
+                      temp=v[j];
+                      v[j]=v[j+mid];
+                      v[j+mid]=temp;
+            }
+            
+        }
+        
+        
+    }
+    return ;
+}
+
+
+void display(int a[], int n)
+{
+    
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return ;
+}
