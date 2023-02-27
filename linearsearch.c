c6d2df1ca77d14cd1175cc4cac84d9143e756750
suvidhaks <32566719+suvidhaks@users.noreply.github.com>
2023-02-27 17:35:14 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,59 @@
+/*
+this program takes time complexity O(n);
+if the key is found at the las loc in an array 
+the entire array has to be traversed to find the key;
+say suppose the number of items in the array are n;
+and if the key is found at the loc [n-1];
+then time complexity requires O(n);
+therefore; we say the that linear search algorithm takes linear time 
+*/
+
+#include <stdio.h>
+#define MAX 100
+
+void read(int a[], int n);
+int linearsearch(int [], int, int);
+
+int main()
+{
+    int a[MAX];
+    int i, n,key;
+    printf("enter n value");
+    scanf("%d", &n);
+    read(a,n);
+    printf("\nenter key value");
+    scanf("%d", &key);
+    i= linearsearch(a, n, key);
+    printf("\n%d", i);
+    return 0;
+}
+
+
+int linearsearch(int a[], int n, int x)
+{
+    int i;
+    
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+void read(int a[], int n)
+{
+    int i;
+    printf("\n enter array elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    return ;
+}
+
+
