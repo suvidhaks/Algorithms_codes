8e4500a5e86e78b89a04c78bf0504bb97791c766
suvidhaks <32566719+suvidhaks@users.noreply.github.com>
2023-06-06 01:18:10 +0530

    Update modular_expo.c

--- modular_expo.c
+++ modular_expo.c
@@ -1,3 +1,15 @@
+/*
+the following code uses repeated square method to find the modular exponent
+ex: y=16; y/2=8;
+2^8 mod 100=56;
+2^4 mod 100= 16; 2^2 mod 100= 4; 2^1 mod 100= 2; 2^0=1 return from function();
+finally functions returns 56^2 mod 100=36;
+if y= 17; odd = 2^16.2
+y/2=16;
+repeat above steps 
+*/
+
+
 #include <stdio.h>
 
 int  modularexpo(int,int,int);
